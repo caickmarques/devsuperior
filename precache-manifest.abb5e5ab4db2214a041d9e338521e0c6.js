cd1e8a6cd7fb2e6699a643e0e4d5796c1f6f4a34
caickmarques <caick_marques@hotmail.com>
2020-09-17 17:33:15 -0300

    Updates

---
+++ precache-manifest.abb5e5ab4db2214a041d9e338521e0c6.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e261dfe4ad57b2093abaf3f8b9cee1bb",
+    "url": "/devsuperior/index.html"
+  },
+  {
+    "revision": "92a4356159ae4faef2ea",
+    "url": "/devsuperior/static/css/main.c7d4d60a.chunk.css"
+  },
+  {
+    "revision": "2dad02a1ef7fe772fe14",
+    "url": "/devsuperior/static/js/2.9bb452bc.chunk.js"
+  },
+  {
+    "revision": "54815be02e01897f642aa6f239b15dec",
+    "url": "/devsuperior/static/js/2.9bb452bc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "92a4356159ae4faef2ea",
+    "url": "/devsuperior/static/js/main.03e57ca5.chunk.js"
+  },
+  {
+    "revision": "48f1e2e7402dda51ac40",
+    "url": "/devsuperior/static/js/runtime-main.4449d1fc.js"
+  },
+  {
+    "revision": "c08814321789f1d603b3a5cc2de108a5",
+    "url": "/devsuperior/static/media/arrow.c0881432.svg"
+  },
+  {
+    "revision": "8d5d159ed7f4dddb22c59da9958c01e3",
+    "url": "/devsuperior/static/media/gamer.8d5d159e.svg"
+  },
+  {
+    "revision": "efb918734459ad437c0e3127c2f97235",
+    "url": "/devsuperior/static/media/logo.efb91873.svg"
+  }
+]);
